b636dbf2dbf1d66259e34825033c1eb7519df09a
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-11-25 15:48:38 +0200

    xml documentation

--- Sets Library/Sets Library/Collections/Element collections/SortedElements.cs
+++ Sets Library/Sets Library/Collections/Element collections/SortedElements.cs
@@ -1,4 +1,27 @@
-﻿using System.Collections;
+﻿/*
+ * File: SortedElements.cs
+ * Author: Phiwokwakhe Khathwane
+ * Date: 24 November 2024
+ * 
+ * Description:
+ * This file defines the SortedElements<T> class, which provides a sorted collection 
+ * of elements that supports operations such as addition, removal, and searching while 
+ * maintaining the sorted order of elements. It extends the BaseSortedCollection<T> class 
+ * and adheres to the ISortedElements<T> interface.
+ * 
+ * Key Features:
+ * - Inherits all core sorted collection functionalities from BaseSortedCollection<T>.
+ * - Provides constructors for initializing the collection with or without an existing set of elements.
+ * - Supports efficient operations while ensuring elements remain sorted.
+ * - Implements the ISortedElements<T> interface for collection-specific behavior.
+ * 
+ * Dependencies:
+ * - SetsLibrary.Collections.BaseSortedCollection<T> for base functionality.
+ * - SetsLibrary.Interfaces.ISortedElements<T> for interface adherence.
+ * - System.Collections for IEnumerable support.
+ */
+
+using System.Collections;
 
 namespace SetsLibrary.Collections
 {
@@ -6,14 +29,19 @@ namespace SetsLibrary.Collections
     /// Represents a collection of sorted elements that allows for addition, removal, and searching within the collection.
     /// </summary>
     /// <typeparam name="T">The type of elements in the collection, which must implement <see cref="IComparable{T}"/>.</typeparam>
-    public class SortedElements<T> : BaseSortedCollection<T> , ISortedElements<T>
+    public class SortedElements<T> : BaseSortedCollection<T>, ISortedElements<T>
         where T : IComparable<T>
     {
-        //Default constructor
-        public SortedElements() : base(){}//ctor main
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedElements{T}"/> class.
+        /// </summary>
+        public SortedElements() : base() { }//ctor main
 
-        //Constructor that initializes the collection with an existing set of elements
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedElements{T}"/> class with an existing set of elements.
+        /// </summary>
+        /// <param name="elements">The collection of elements to initialize the sorted collection with.</param>
         public SortedElements(IEnumerable<T> elements)
-            : base(elements){}//ctor 01
+            : base(elements) { }//ctor 01
     }//class
 } //namespace SetLibrary.Collections
