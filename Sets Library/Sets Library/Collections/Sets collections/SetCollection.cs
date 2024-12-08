23eb0ba92a3de9a199193d6c718203f501137226
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-12-08 12:35:15 +0200

    Documentation

--- Sets Library/Sets Library/Collections/Sets collections/SetCollection.cs
+++ Sets Library/Sets Library/Collections/Sets collections/SetCollection.cs
@@ -1,31 +1,72 @@
-﻿using SetsLibrary.Collections;
+﻿/*
+ * File: SetCollection.cs
+ * Author: Phiwokwakhe Khathwane
+ * Date: 8 December 2024
+ * 
+ * Description:
+ * This file contains the definition of the SetCollection<T> class, 
+ * a generic collection that holds unique sets, identified by string keys. 
+ * It provides methods to add, remove, check, and retrieve sets by their names, 
+ * as well as functionalities to reset and iterate over the collection.
+ * 
+ * Key Features:
+ * - Stores sets of elements that implement IComparable.
+ * - Each set is identified by a unique string key, generated sequentially.
+ * - Supports adding individual sets, adding a range of sets, and clearing the collection.
+ * - Provides methods for checking the existence of sets by name or by object reference.
+ * - Allows for enumerating over the collection of sets.
+ * 
+ * Generic Constraints:
+ * - T must implement IComparable<T> to ensure ordered comparison of elements within the sets.
+ */
+
+using SetsLibrary.Collections;
 using SetsLibrary.Interfaces;
 using System.Collections;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 
 namespace SetLibrary.Collections
 {
+    /// <summary>
+    /// A generic collection class that holds a set of unique sets, identified by string keys.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the sets, which must implement IComparable.</typeparam>
     public class SetCollection<T> : ISetCollection<IStructuredSet<T>, T>
         where T : IComparable<T>
     {
-        //Data-fields
-        //This dictionary will hold the set names(Unique) as well as the set itselt 
+        // Data-fields
+        // This dictionary will hold the set names (Unique) as well as the set itself.
         private Dictionary<string, IStructuredSet<T>> _dicCollection;
-        //This will keep track of the current key/last key
+
+        // This will keep track of the current key/last key.
         private Key _lastKey;
 
         #region Embedded class
+        /// <summary>
+        /// Represents a key used to uniquely identify sets within the collection.
+        /// </summary>
         private class Key : IEqualityComparer<Key>
         {
             private readonly List<char> _keyChars;
 
+            /// <summary>
+            /// Gets the full key value as a string.
+            /// </summary>
             public string FullKeyValue { get; private set; }
+
+            /// <summary>
+            /// Default constructor initializing an empty key.
+            /// </summary>
             public Key()
             {
                 _keyChars = new List<char>();
                 FullKeyValue = "";
             }
+
+            /// <summary>
+            /// Initializes a key with a list of character values.
+            /// </summary>
+            /// <param name="characterKeys">The collection of characters representing the key.</param>
             public Key(IEnumerable<char> characterKeys)
                 : this()
             {
@@ -34,15 +75,19 @@ namespace SetLibrary.Collections
                 _keyChars.AddRange(characterKeys);
 
                 FullKeyValue = string.Join("", characterKeys);
-            }//ctor main
+            }
+
+            /// <summary>
+            /// Generates the next sequential key by incrementing the current key value.
+            /// </summary>
             public Key GenerateNextKey()
             {
-                //If the current list is empty
+                // If the current list is empty
                 if (_keyChars.Count == 0)
                     return new Key("A");
 
-                //Create a copy
-                //Check the last and first elements if they are 'Z'
+                // Create a copy
+                // Check the last and first elements if they are 'Z'
                 char[]? newValues = null;
 
                 if (_keyChars[0] == 'Z' && _keyChars[_keyChars.Count - 1] == 'Z')
@@ -50,7 +95,7 @@ namespace SetLibrary.Collections
                 else
                     newValues = new char[_keyChars.Count];
 
-                //Here the length of the array is already been determined
+                // Here the length of the array is already been determined
                 bool isIncremented = false;
                 for (int i = _keyChars.Count - 1; i >= 0; i--)
                 {
@@ -66,12 +111,15 @@ namespace SetLibrary.Collections
                     newValues[i] = current;
                 }
 
-                if (_keyChars.Count < newValues.Length)//It means that the values array has one extra slot
+                if (_keyChars.Count < newValues.Length) // It means that the values array has one extra slot
                     newValues[newValues.Length - 1] = 'A';
 
                 return new Key(newValues);
-            }//GenerateNextKey
-            //Reset key
+            }
+
+            /// <summary>
+            /// Resets the key to an empty state.
+            /// </summary>
             public void ResetKey()
             {
                 _keyChars.Clear();
@@ -86,12 +134,19 @@ namespace SetLibrary.Collections
             {
                 return obj.FullKeyValue.GetHashCode();
             }
-        }//emebed class
+        } // Embedded class
         #endregion Embedded class
-        //Properties
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the count of sets in the collection.
+        /// </summary>
         public int Count => _dicCollection.Count;
 
-        //inderxer
+        /// <summary>
+        /// Indexer to access sets by their name.
+        /// </summary>
         public IStructuredSet<T>? this[string name]
         {
             get
@@ -100,92 +155,132 @@ namespace SetLibrary.Collections
             }
         }
 
+        #endregion Properties
 
         #region Constructors
 
+        /// <summary>
+        /// Default constructor that initializes an empty collection.
+        /// </summary>
         public SetCollection()
         {
             Clear();
-        }//ctor default
+        }
 
+        /// <summary>
+        /// Initializes the collection with a range of sets.
+        /// </summary>
+        /// <param name="collection">The collection of sets to add.</param>
         public SetCollection(IEnumerable<IStructuredSet<T>> collection)
         {
             Clear();
-            
-            //Check for nulls
+
+            // Check for nulls
             ArgumentNullException.ThrowIfNull(collection, nameof(collection));
 
-            //Add the range
+            // Add the range
             this.AddRange(collection);
         }
 
         #endregion Constructors
 
+        #region Methods
 
-
+        /// <summary>
+        /// Adds a single set to the collection.
+        /// </summary>
+        /// <param name="item">The set to add.</param>
         public void Add(IStructuredSet<T> item)
         {
             _lastKey = _lastKey.GenerateNextKey();
 
-            //Check for nulls
+            // Check for nulls
             ArgumentNullException.ThrowIfNull(item, nameof(item));
 
-            //Add the element
+            // Add the element
             _dicCollection.Add(_lastKey.FullKeyValue, item);
         }
+
+        /// <summary>
+        /// Adds multiple sets to the collection.
+        /// </summary>
+        /// <param name="collection">The collection of sets to add.</param>
         public void AddRange(IEnumerable<IStructuredSet<T>> collection)
         {
-            //Check for nulls
+            // Check for nulls
             ArgumentNullException.ThrowIfNull(collection, nameof(collection));
 
             foreach (var item in collection)
                 Add(item);
-        }//AddRange
+        }
 
+        /// <summary>
+        /// Clears the collection, removing all sets.
+        /// </summary>
         public void Clear()
         {
             _dicCollection = new Dictionary<string, IStructuredSet<T>>();
             _lastKey = new Key();
         }
 
+        /// <summary>
+        /// Checks if a set is present in the collection.
+        /// </summary>
+        /// <param name="item">The set to check.</param>
+        /// <returns>True if the set is in the collection; otherwise, false.</returns>
         public bool Contains(IStructuredSet<T> item)
         {
-            //Check for nulls
+            // Check for nulls
             ArgumentNullException.ThrowIfNull(item, nameof(item));
 
-            //USe linq to check for it
+            // Use LINQ to check for it
             return _dicCollection.ContainsValue(item);
-        }//Contains
+        }
 
+        /// <summary>
+        /// Checks if a set with a given name exists in the collection.
+        /// </summary>
+        /// <param name="name">The name of the set to check for.</param>
+        /// <returns>True if a set with the name exists; otherwise, false.</returns>
         public bool Contains(string name)
         {
-            //Check for nulls
+            // Check for nulls
             ArgumentNullException.ThrowIfNullOrWhiteSpace(name, nameof(name));
 
-            //Get the key value
+            // Get the key value
             Key key = new Key(name);
 
             return _dicCollection.ContainsKey(key.FullKeyValue);
         }
 
+        /// <summary>
+        /// Finds a set by its name.
+        /// </summary>
+        /// <param name="name">The name of the set.</param>
+        /// <returns>The set if found, otherwise null.</returns>
         public IStructuredSet<T>? FindSetByName(string name)
         {
-            //Check for nulls
+            // Check for nulls
             ArgumentNullException.ThrowIfNullOrWhiteSpace(name, nameof(name));
 
-            //Generate the key
+            // Generate the key
             Key key = new Key(name);
 
-            //Check if the key exists
-            if( _dicCollection.ContainsKey(key.FullKeyValue))
+            // Check if the key exists
+            if (_dicCollection.ContainsKey(key.FullKeyValue))
                 return _dicCollection[key.FullKeyValue];
 
             return null;
-        }//FindSetByName
+        }
 
+        /// <summary>
+        /// Removes a set by its name.
+        /// </summary>
+        /// <param name="name">The name of the set to remove.</param>
+        /// <returns>True if the set was removed; otherwise, false.</returns>
         public bool Remove(string name)
         {
-            //Check for nulls
+            // Check for nulls
             ArgumentNullException.ThrowIfNullOrWhiteSpace(name, nameof(name));
 
             Key key = new Key(name);
@@ -193,19 +288,25 @@ namespace SetLibrary.Collections
             return _dicCollection.Remove(key.FullKeyValue);
         }
 
+        /// <summary>
+        /// Resets the collection to its initial state, re-adding all sets that were previously added.
+        /// </summary>
         public void Reset()
         {
-            //Make a copy of the list
+            // Make a copy of the list
             var array = _dicCollection.Values;
 
-            //Reset the current clasas
+            // Reset the current class
             Clear();
 
-            //Re-add the sets
+            // Re-add the sets
             AddRange(array);
-            //
-        }//Reset
+        }
 
+        /// <summary>
+        /// Returns an enumerator for the collection.
+        /// </summary>
+        /// <returns>An enumerator for the collection.</returns>
         public IEnumerator<KeyValuePair<string, IStructuredSet<T>>> GetEnumerator()
         {
             foreach (var item in _dicCollection)
@@ -214,9 +315,15 @@ namespace SetLibrary.Collections
             }
         }
 
+        /// <summary>
+        /// Returns a non-generic enumerator for the collection.
+        /// </summary>
+        /// <returns>A non-generic enumerator for the collection.</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
         }
-    }//class
-}//namespace
+
+        #endregion Methods
+    } // class
+} // namespace
