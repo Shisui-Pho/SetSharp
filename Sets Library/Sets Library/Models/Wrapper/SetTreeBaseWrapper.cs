7d9fc6dfcf07aae8608d5bebda31cbcf45ec06c2
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-12-08 14:10:20 +0200

    Added a base wrapper and a concrete wrapper for the SetTree structure

---
+++ Sets Library/Sets Library/Models/Wrapper/SetTreeBaseWrapper.cs
@@ -0,0 +1,82 @@
+﻿using SetsLibrary.Interfaces;
+namespace SetsLibrary.Models;
+
+public abstract class SetTreeBaseWrapper<T> : ISetTree<T>
+    where T : IComparable<T>
+{
+    protected readonly ISetTree<T> setTree;
+
+    public SetTreeBaseWrapper(ISetTree<T> setTree)
+    {
+        //Check for nulls
+        ArgumentNullException.ThrowIfNull(setTree, nameof(setTree));
+
+
+        this.setTree = setTree;
+    }//ctor default
+
+    public string RootElements => setTree.RootElements;
+
+    public int Count => setTree.Count;
+
+    public int CountRootElements => setTree.CountRootElements;
+
+    public int CountSubsets => setTree.CountSubsets;
+
+    public SetExtractionConfiguration<T> ExtractionSettings => setTree.ExtractionSettings;
+
+    public void AddElement(T element)
+    {
+        setTree.AddElement(element);
+    }
+
+    public void AddRange(IEnumerable<T> elements)
+    {
+        setTree.AddRange(elements);
+    }
+
+    public void AddRange(IEnumerable<ISetTree<T>> subsets)
+    {
+        setTree.AddRange(subsets);
+    }
+
+    public void AddSubSetTree(ISetTree<T> tree)
+    {
+        setTree.AddSubSetTree(tree);
+    }
+
+    public int CompareTo(ISetTree<T>? other)
+    {
+        return setTree.CompareTo(other);
+    }
+
+    public IEnumerable<T> GetRootElementsEnumarator()
+    {
+        return setTree.GetRootElementsEnumarator();
+    }
+
+    public IEnumerable<ISetTree<T>> GetSubsetsEnumarator()
+    {
+        return setTree.GetSubsetsEnumarator();
+    }
+
+    public int IndexOf(T element)
+    {
+        return setTree.IndexOf(element);
+    }
+
+    public int IndexOf(ISetTree<T> subset)
+    {
+        return setTree.IndexOf(subset);
+    }
+
+    public bool RemoveElement(T element)
+    {
+        return setTree.RemoveElement(element);
+    }
+
+    public bool RemoveElement(ISetTree<T> element)
+    {
+        return setTree.RemoveElement(element);
+    }
+}//class
---
+++ Sets Library/Sets Library/Models/Wrapper/SetTreeWrapper.cs
@@ -0,0 +1,64 @@
+﻿using SetsLibrary.Interfaces;
+using SetsLibrary.Models.SetTree;
+namespace SetsLibrary.Models;
+
+public class SetTreeWrapper<T> : SetTreeBaseWrapper<T>
+    where T: IComparable<T>
+{
+    private readonly SetTree<T>? _assSetTree = null;
+    public SetTreeWrapper(ISetTree<T> setTree): base(setTree) 
+    {
+        if(setTree is SetTree<T>)//if they are the same cast the setTree structure
+            _assSetTree = setTree as SetTree<T>;
+
+    }//ctor main
+
+    public T? GetRootElementByIndex(int index)
+    {
+        if (index >= Count || index < 0)
+            throw new ArgumentOutOfRangeException("index");
+
+        if(_assSetTree != null)
+            return _assSetTree._elements[index];//Acces direcly from the internal colllection
+
+        //Else do a loop
+        int currentIndex = 0;
+        foreach(T item in base.GetRootElementsEnumarator())
+        {
+            if(currentIndex ==  index)
+                return item;
+
+            currentIndex++;
+        }
+        return default(T);
+    }//GetRootElementByIndex
+    public ISetTree<T>? GetSubsetByIndex(int index)
+    {
+        if (index >= Count || index < 0)
+            throw new ArgumentOutOfRangeException("index");
+
+        //Else do a loop
+        int currentIndex = 0;
+        foreach (ISetTree<T> item in base.GetSubsetsEnumarator())
+        {
+            if (currentIndex == index)
+                return item;
+
+            currentIndex++;
+        }
+        return default(ISetTree<T>);
+    }//GetSubsetByIndex
+    public void Clear()
+    {
+        if(_assSetTree != null)
+        {
+            _assSetTree._elements.Clear();
+            _assSetTree._subSets.Clear();
+        }
+        else
+        {
+            throw new ArgumentException("Use a seperate wrapper for this operation or wrapp this one.");
+        }
+    }//Clear
+
+}//class
